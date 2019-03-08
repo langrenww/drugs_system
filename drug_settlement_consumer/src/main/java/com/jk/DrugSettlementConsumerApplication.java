4aa51bc5668a96ff5ce6770b0c7d8b292d4ae16f
mawenlong2001 <2251400549@qq.com>
2019-03-08 14:31:39 +0800

    马文龙第一次提交框架

---
+++ drug_settlement_consumer/src/main/java/com/jk/DrugSettlementConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugSettlementConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugSettlementConsumerApplication.class, args);
+    }
+
+}
---
+++ drug_settlement_service/src/main/java/com/jk/DrugSettlementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugSettlementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugSettlementServiceApplication.class, args);
+    }
+
+}
