a6f5b81aae5059ff218503ac623fda163fb1db5f
guodinghuaa <2503247724@qq.com>
2019-03-08 22:18:46 +0800

    郭丁华上传供应商药品消费者

--- drug_supplier_customer/src/main/java/com/jk/controller/CustomerController.java
+++ drug_supplier_customer/src/main/java/com/jk/controller/CustomerController.java
@@ -1,12 +1,26 @@
 package com.jk.controller;
 
-import com.jk.service.CustomerService;
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.jk.pojo.SupplierDrug;
+import com.jk.service.CustomerServiceFegin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class CustomerController {
     @Autowired
-    private CustomerService customerService;
+    private CustomerServiceFegin customerService;
+
+    @GetMapping("customerFind")
+    @ResponseBody
+    public List<SupplierDrug> SupperDrugFind(Integer supplierId){
+        supplierId=2;
+        return customerService.SupperDrugFind(supplierId);
+    }
+
 
 }
--- drug_supplier_customer/src/main/java/com/jk/service/CustomerService.java
+++
@@ -1,8 +0,0 @@
-package com.jk.service;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient("supplier-server")//声明为feig客户端 指明要调用的服务
-public interface CustomerService extends SupplierApi{
-
-}
