a0e6803813a5cfe11e543fa6a8088990939377bc
songwenhuis <826265621@qq.com>
2019-03-08 13:39:39 +0800

    宋文辉提交API接口

---
+++ drug_agency_api/src/main/java/com/jk/DrugAgencyApiApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugAgencyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugAgencyApiApplication.class, args);
+    }
+
+}
