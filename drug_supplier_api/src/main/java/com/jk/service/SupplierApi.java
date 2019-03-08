bb537397340fead43552403baa5c91d23d737efe
guodinghuaa <2503247724@qq.com>
2019-03-08 22:14:32 +0800

    上传供应商药品Api接口

--- drug_supplier_api/src/main/java/com/jk/service/SupplierApi.java
+++ drug_supplier_api/src/main/java/com/jk/service/SupplierApi.java
@@ -1,4 +1,13 @@
 package com.jk.service;
 
+
+import com.jk.pojo.SupplierDrug;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
 public interface SupplierApi {
+    @GetMapping
+   List<SupplierDrug> SupperDrugFind(@RequestParam("supplierId") Integer supplierId);
 }
