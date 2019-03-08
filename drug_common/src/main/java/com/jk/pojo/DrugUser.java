e6c13bcfde0a919928f2f01a92d5d6cda10e534a
cl <1509955265@qq.com>
2019-03-08 14:03:07 +0800

    陈磊第一次提交实体

---
+++ drug_common/src/main/java/com/jk/pojo/DrugUser.java
@@ -0,0 +1,49 @@
+package com.jk.pojo;
+
+public class DrugUser {
+    private Integer userId;
+    private String userName;
+    private String userPassword;
+    private String userAccoountNumber;
+    private String userPhoneNumber;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserAccoountNumber() {
+        return userAccoountNumber;
+    }
+
+    public void setUserAccoountNumber(String userAccoountNumber) {
+        this.userAccoountNumber = userAccoountNumber;
+    }
+
+    public String getUserPhoneNumber() {
+        return userPhoneNumber;
+    }
+
+    public void setUserPhoneNumber(String userPhoneNumber) {
+        this.userPhoneNumber = userPhoneNumber;
+    }
+}
