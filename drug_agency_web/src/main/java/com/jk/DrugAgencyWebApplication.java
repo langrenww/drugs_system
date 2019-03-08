fc67db6a151700852d2e27083810a75b8b97bf80
songwenhuis <826265621@qq.com>
2019-03-08 16:46:39 +0800

    1.0.6版本 宋文辉提交 web 完善启动类

--- drug_agency_web/src/main/java/com/jk/DrugAgencyWebApplication.java
+++ drug_agency_web/src/main/java/com/jk/DrugAgencyWebApplication.java
@@ -2,11 +2,16 @@ package com.jk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class DrugAgencyWebApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DrugAgencyWebApplication.class, args);
     }
 
