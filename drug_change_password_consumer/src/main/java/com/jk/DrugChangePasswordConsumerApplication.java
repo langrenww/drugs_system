daa96f1cc0a9332fa4dea3868758d6b924019a23
cl <1509955265@qq.com>
2019-03-08 14:01:03 +0800

    陈磊第一次提交消费者

---
+++ drug_change_password_consumer/src/main/java/com/jk/DrugChangePasswordConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugChangePasswordConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugChangePasswordConsumerApplication.class, args);
+    }
+
+}
