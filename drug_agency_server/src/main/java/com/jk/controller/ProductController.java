d9f96222c4e11b24ebbe4ca0da97e45b853821f0
songwenhuis <826265621@qq.com>
2019-03-09 14:20:06 +0800

    1.0.7版本 宋文辉提交 server 提交业务层修改

--- drug_agency_server/src/main/java/com/jk/controller/ProductController.java
+++ drug_agency_server/src/main/java/com/jk/controller/ProductController.java
@@ -1,10 +1,16 @@
 package com.jk.controller;
 
+import com.jk.pojo.ProductAgency;
+import com.jk.pojo.ProductAgencyDto;
 import com.jk.service.ProductService;
 import com.jk.service.ProductServiceApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class ProductController implements ProductServiceApi {
@@ -17,5 +23,24 @@ public class ProductController implements ProductServiceApi {
         String str=productService.frameTest();
         System.out.println("已进入到server");
         System.out.println(str);
+    }
+    @ResponseBody
+    @Override
+    public String saveAgencyInfo(@RequestBody ProductAgency productAgency) {
+        int i = productService.saveAgencyInfo(productAgency);
+        String str="";
+        if(i!=0){
+             str="添加成功";
+        }else{
+             str="添加失败";
+        }
+        return str;
+    }
+    @GetMapping("queryAgencyInfo")
+    @ResponseBody
+    @Override
+    public List<ProductAgencyDto> queryAgencyInfo() {
+
+        return productService.queryAgencyInfo();
     }
 }
