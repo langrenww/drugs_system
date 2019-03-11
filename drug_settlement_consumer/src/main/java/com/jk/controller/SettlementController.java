80978296883669a873f9fdbb440aaeacf83bea8b
mawenlong2001 <2251400549@qq.com>
2019-03-11 14:24:24 +0800

    马文龙第二次提交框架

--- drug_settlement_consumer/src/main/java/com/jk/controller/SettlementController.java
+++ drug_settlement_consumer/src/main/java/com/jk/controller/SettlementController.java
@@ -1,10 +1,13 @@
 package com.jk.controller;
 
+import com.jk.pojo.BankCard;
 import com.jk.pojo.Test;
 import com.jk.service.SettlementServiceConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -15,11 +18,31 @@ public class SettlementController {
     @Autowired
     private SettlementServiceConsumer settlementServiceConsumer;
 
+    /**
+     * 测试框架
+     * @return
+     */
     @GetMapping("test")
     @ResponseBody
     public List<Test> test(){
         List<Test> list=  settlementServiceConsumer.test();
        return  list;
+    }
+
+    /**
+     * 绑卡设置 添加银行卡
+     * @param bankCard
+     */
+    @PostMapping("saveBankCard")
+    @ResponseBody
+    public void saveBankCard(@RequestBody BankCard bankCard){
+        settlementServiceConsumer.saveBankCard(bankCard);
+
+    }
+    @GetMapping("querySettlement")
+    @ResponseBody
+    public void querySettlement(){
+
     }
 
 }
