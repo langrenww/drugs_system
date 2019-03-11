427a331fca882cb4afbc47b543934d825d62f926
cl <1509955265@qq.com>
2019-03-11 11:02:24 +0800

    陈磊第一次提交查询发布状态接口

---
+++ drug_queryissuedstatus_api/src/main/java/com/jk/service/QueryDrugByIssuedStatusServiceApi.java
@@ -0,0 +1,28 @@
+package com.jk.service;
+
+import com.jk.pojo.SupplierDrug;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public interface QueryDrugByIssuedStatusServiceApi {
+
+    @PostMapping("QueryDrugByIssuedStatus")
+    List<SupplierDrug> QueryDrugByIssuedStatus(SupplierDrug supplierDrug);
+    @PutMapping("updateIssuedStatus")
+    void updateIssuedStatus(String[] ids);
+    @PutMapping("updateIssuedStatus2")
+    void updateIssuedStatus2(String[] ids);
+    @DeleteMapping("deleteDrugById")
+    void deleteDrugById(String id);
+    @GetMapping("queryDrugById")
+    List<SupplierDrug> queryDrugById(@RequestParam("id") String id);
+    @PostMapping("updateDrug")
+    void updateDrug(SupplierDrug supplierDrug);
+    @PostMapping("saveDrug")
+    void saveDrug(SupplierDrug supplierDrug);
+    @PostMapping("QueryDrugBySpreadStatus")
+    List<SupplierDrug> QueryDrugBySpreadStatus(SupplierDrug supplierDrug);
+    @PostMapping("QueryDrugBySpreadStatus2")
+    List<SupplierDrug> QueryDrugBySpreadStatus2(SupplierDrug supplierDrug);
+}
