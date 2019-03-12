b5acd356c3483ca2c95093c2d55a477f1158c9bb
songwenhuis <826265621@qq.com>
2019-03-12 08:46:15 +0800

    1.0.7版本 宋文辉提交 server

--- drug_agency_web/src/main/java/com/jk/controller/ProductController.java
+++ drug_agency_web/src/main/java/com/jk/controller/ProductController.java
@@ -22,16 +22,35 @@ public class ProductController {
         System.out.println("已进入到web");
         return null;
     }
+
+    /**
+     * 添加代理信息
+     * @param productAgency
+     * @return
+     */
     @PostMapping("saveAgencyInfo")
     @ResponseBody
     public String saveAgencyInfo(@RequestBody ProductAgency productAgency) {
         String str = productService.saveAgencyInfo(productAgency);
         return str;
     }
+    //查询地区代理人和信息
     @GetMapping("queryAgencyInfo")
     @ResponseBody
     public List<ProductAgencyDto> queryAgencyInfo() {
 
         return productService.queryAgencyInfo();
+    }
+
+    /**
+     * 删除单条代理信息
+     * @param id
+     * @return
+     */
+    @DeleteMapping("deleteAgencyInfo")
+    @ResponseBody
+    public String deleteAgencyInfo(String  id){
+       String str= productService.deleteAgencyInfo(id);
+        return str;
     }
 }
