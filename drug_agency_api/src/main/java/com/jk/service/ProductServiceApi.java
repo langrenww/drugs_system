83eaf98217a05f304d261f2290b119413d40ed2f
songwenhuis <826265621@qq.com>
2019-03-20 14:02:52 +0800

    1.0.7版本 宋文辉提交 server

--- drug_agency_api/src/main/java/com/jk/service/ProductServiceApi.java
+++ drug_agency_api/src/main/java/com/jk/service/ProductServiceApi.java
@@ -1,20 +1,34 @@
 package com.jk.service;
 
-import com.jk.pojo.ProductAgency;
-import com.jk.pojo.ProductAgencyDto;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import com.jk.pojo.*;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 public interface ProductServiceApi {
     @GetMapping
     void frameTest();
-    @PostMapping
+    @PostMapping("saveAgencyInfo")
     String saveAgencyInfo(ProductAgency productAgency);
     @GetMapping("queryAgencyInfo")
     List<ProductAgencyDto> queryAgencyInfo();
     @DeleteMapping("deleteAgencyInfo")
     String deleteAgencyInfo(String  id);
+    @PostMapping  ("toConnect")
+    List<District> selectDirect(@RequestBody  String pid);
+    @PostMapping("submitAuditInfo")
+    String submitAuditInfo(EnterpriseQualificationDetail enterpriseQualificationDetail);
+    @PutMapping("updateAgencyInfo")
+    String updateAgencyInfo(@RequestBody String id);
+    @GetMapping("approveAgencyInfo")
+    List<ProductAgencyDto> approveAgencyInfo();
+
+    @GetMapping  ("queryGoodsById")
+    List<Goods> queryGoodsById(@RequestParam("pid") Integer pid);
+    @GetMapping("queryNewGoods")
+    List<NewestGoods> queryNewGoods();
+    @PostMapping("addSupplyGoods")
+    void addSupplyGoods(@RequestBody ShoppingInfo shoppingInfo);
+    @GetMapping("querySupplyGoods")
+    List<ShoppingInfo> querySupplyGoods();
 }
